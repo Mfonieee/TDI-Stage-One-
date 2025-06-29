9ec32447556a84b7b1d1f95b75ba4bf1821ba9d8
omijieprecious <omijieprecious09@gmail.com>
2025-06-29 23:08:45 +0100

    Added missing folder and files

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ import streamlit as st
 from dashboard import run_dashboard
 from prediction import run_prediction
 
+# Set page config (must be first Streamlit command)
 st.set_page_config(page_title="Loan Dashboard", page_icon="📊", layout="wide")
 
-# Navigation
+# Load and display logo and title
+logo_path = "image\logo.png" 
+
+# Header layout
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.image(logo_path, width=80)
+with col2:
+    st.markdown(
+        """
+        <h1 style='padding-top: 10px; color: #c7522a; font-size: 32px;'>TDI Trust Bank</h1>
+        """,
+        unsafe_allow_html=True
+    )
+
+st.markdown("---")
+
+# Navigation Sidebar
 page = st.sidebar.radio("Navigation", ["Dashboard", "Predict Loan Approval"])
 
 if page == "Dashboard":

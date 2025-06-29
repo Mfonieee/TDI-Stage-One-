70a63ebf5832d3f437a5619c4c98e4c62617e68c
IdUsoro22 <109907358+IdUsoro22@users.noreply.github.com>
2025-06-29 07:03:18 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from dashboard import run_dashboard
+from prediction import run_prediction
+
+st.set_page_config(page_title="Loan Dashboard", page_icon="📊", layout="wide")
+
+# Navigation
+page = st.sidebar.radio("Navigation", ["Dashboard", "Predict Loan Approval"])
+
+if page == "Dashboard":
+    run_dashboard()
+elif page == "Predict Loan Approval":
+    run_prediction()
